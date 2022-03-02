b17cd39dcd7ea551912c44d17092d6161fbbe224
Alfredo Torres <amtrosul@hotmail.com>
2022-03-02 15:28:26 -0300

    postgre config

--- src/main/java/com/eventoapp/DatabaseConfigurationMySql.java
+++ src/main/java/com/eventoapp/DatabaseConfigurationMySql.java
@@ -4,13 +4,14 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-
-public class DatabaseConfiguration {
+// @Configuration
+public class DatabaseConfigurationMySql {
 
 	@Bean
 	public DataSource dataSource() {
