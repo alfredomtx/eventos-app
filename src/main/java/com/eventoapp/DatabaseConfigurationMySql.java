a53dab3e116f4231db21f3b3515cc7af752aeee2
Alfredo Torres <amtrosul@hotmail.com>
2022-03-02 15:22:54 -0300

    Revert "postgre configuration"
    This reverts commit b01127ef195478d3f5e5460046bcebf95e78f32f.

--- src/main/java/com/eventoapp/DatabaseConfigurationMySql.java
+++ src/main/java/com/eventoapp/DatabaseConfigurationMySql.java
@@ -3,7 +3,6 @@ package com.eventoapp;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
