f5cab2da6d86e6262f388df9cfbb74a765795ce3
Alfredo Torres <amtrosul@hotmail.com>
2022-03-02 14:30:39 -0300

    configuring postgre sql heroku

---
+++ src/main/java/com/eventoapp/DatabaseConfigurationPostgreSql.java
@@ -0,0 +1,42 @@
+package com.eventoapp;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DatabaseConfigurationPostgreSql {
+
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("isyrfzlckziunw:211a6f24f2719d9d0d3bfcaffecdbeb6973590efc3506e2180d1b27b88911ebc@ec2-34-206-148-196.compute-1.amazonaws.com:5432/dae0h20m4t1ff2"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.POSTGRESQL);
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+
+}
